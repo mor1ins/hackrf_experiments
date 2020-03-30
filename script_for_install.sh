c9af58a1fec26a8e47025b3aacfab045ae7730cc
Eduard Kuliev <kuliev.ed@gmail.com>
2020-03-30 11:23:02 +0300

    добавил скрипт для установки sigintos

---
+++ script_for_install.sh
@@ -0,0 +1,9 @@
+sudo mkdir /target/boot
+
+sudo cp /cdrom/casper/initrd.gz /target/boot/initrd.gz
+sudo cp /cdrom/casper/vmlinuz /boot/vmlinuz
+
+sudo cp /cdrom/casper/vmlinuz /target/boot/vmlinuz
+sudo cp /cdrom/casper/vmlinuz /boot/vmlinuz-4.15.0-43-generic
+
+sudo cp /cdrom/casper/vmlinuz /target/boot/vmlinuz-4.15.0-43-generic
